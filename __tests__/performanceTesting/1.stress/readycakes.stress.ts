f52ac3933cd579e7269d11696968fe92b5d349bc
Defla23 <deflachebet321@gmail.com>
2025-11-05 16:21:39 +0300

    added performance testing

---
+++ __tests__/performanceTesting/1.stress/readycakes.stress.ts
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+
+export const options = {
+    stages: [
+        { duration: '10m', target: 200 }, 
+        { duration: '20m', target: 200 }, 
+        { duration: '10m', target: 0 },   
+    ]
+};
+
+export default () => {
+    const res = http.get('http://localhost:5000/readycakes');
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
+}
+sleep(1);
