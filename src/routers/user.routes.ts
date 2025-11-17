f4bf4d90013acae24b75c5b80306391d24da269e
liz-dot406 <njokimunywa@gmail.com>
2025-11-17 17:04:25 +0300

    fixed one more bug

--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -3,12 +3,7 @@ import * as userController from "../Controllers/user.controller";
 import { adminOnly, userOnly, } from "../middlewares/auth.middlewares";
 
 export default function registerUserRoutes(app: Application) {
-<<<<<<< HEAD
- 
-  app.post("/users/register", adminOnly, userController.createUserController);
-=======
   app.post("/users/register", userController.createUserController);
->>>>>>> 31b98b096ac76b33ab7b6f5b800f5d68d860d9aa
   app.post("/users/login", userController.loginUserController);
   app.get("/users", userController.getAllUsersController);
   app.get("/users/:id", userController.getUserByIdController);
